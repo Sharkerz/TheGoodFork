0ebcb145cc6fcce09a4c3b6330cd5a78e29ee7bc
Gandalf0u <leo.auvray@free.fr>
2021-05-19 16:59:23 +0200

    fix conflict

--- mobile/App.js
+++ mobile/App.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import RegistrationScreen from './app/assets/screen/RegistrationScreen'
 import WelcomeScreen from './app/assets/screen/WelcomeScreen'
-=======
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import WelcomeScreen from './app/screen/WelcomeScreen';
->>>>>>> bea463de1aa1afd531f33aa3ead08fc3647fce24
 
 export default function App() {
   return (
