1dba118b8d2cc1b353929fa31fb75127f8252a33
sebcrn <44265088+sebcrn@users.noreply.github.com>
2021-05-19 15:37:39 +0200

    Test home page design

--- mobile/App.js
+++ mobile/App.js
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './app/assets/screen/WelcomeScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Wsh la team</Text>
-      <StatusBar style="auto" />
-    </View>
+  <WelcomeScreen/>  
   );
 }
 
