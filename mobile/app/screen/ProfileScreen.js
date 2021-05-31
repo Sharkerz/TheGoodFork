4db6b1795ce2f2ae7a9bf6401fa1c2cd600299f6
Sharkerz <maxence.lux@supinfo.com>
2021-05-31 13:11:08 +0200

    🚧 working profile page

--- mobile/app/screen/ProfileScreen.js
+++ mobile/app/screen/ProfileScreen.js
@@ -1,9 +1,6 @@
 import React, {Component} from 'react';
-import {ImageBackground, StyleSheet, View, Text, Image, DevSettings} from 'react-native';
-import Background from '../components/Background'
+import {ImageBackground, StyleSheet, View, Text, Image, DevSettings, FlatList} from 'react-native';
 import Button from '../components/Button'
-import Paragraph from '../components/Paragraph'
-import BackButton from '../components/BackButton'
 import * as SecureStore from "expo-secure-store";
 
 class ProfileScreen extends Component {
@@ -13,12 +10,64 @@ class ProfileScreen extends Component {
     DevSettings.reload();
   }
 
+  navigateFromList = () => {
+    //this.props.navigation
+  }
+
+  list = [
+    {
+      key: '🎟️ Points de fidelité',
+      val: '45',
+      navigable: false
+    },
+    {
+      key: '🍽️ Visites',
+      val: '6',
+      navigable: false
+    },
+    {
+      key: '🍕 Nourriture',
+      val: '8',
+      navigable: false
+    },
+    {
+      key: '🍺 Boissons',
+      val: '22',
+      navigable: false
+    },
+    {
+      title: 'Editer mon profil',
+      navigable: true
+    }
+  ]
+
   render() {
     return (
         <ImageBackground style={styles.container} source={require("../assets/background2.png")} >
-          <Paragraph style={styles.textHome}>
-            Profile
-          </Paragraph>
+
+          <FlatList style={styles.list}
+              data={this.list}
+              renderItem={({item}) =>
+                {
+                  if (item.navigable) {
+                    return <View style={styles.item}>
+                            <Text onPress={() => this.navigateFromList(item.title)} style={styles.textRowList}>{item.title}</Text>
+                           </View>
+                  }
+                  else {
+                    return <View style={styles.item}>
+                      <View style={styles.leftViewItem}>
+                        <Text style={styles.textRowList}>{item.key}</Text>
+                      </View>
+                      <View style={styles.rightViewItem}>
+                        <Text style={styles.textRowList}>{item.val}</Text>
+                      </View>
+                          </View>
+                  }
+                }
+              }
+              />
+
           <Button color='#111219' style={styles.textRegister}
                   mode="outlined" onPress={() => this.logout()}>
             Se déconnecter
@@ -31,50 +80,37 @@ class ProfileScreen extends Component {
 export default ProfileScreen
 
 const styles = StyleSheet.create({
-  textHome: {
-    fontSize: 40,
-    textAlign: 'center',
-    top: 420,
-    marginBottom: 0,
-    color: "#fff",
-    fontWeight: "600"
-  },
-  logo: {
-    width: 300,
-    height: 226,
-    position: 'absolute',
-    top: 150,
-  },
-  logoContainer: {
-    width: 200,
-    position: 'absolute',
-    alignItems: 'center'
-  },
   container: {
     flex: 1,
-    alignItems: 'center'
-  },
-  header: {
-    flex: 2,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  footer: {
-    backgroundColor: '#fff',
-    flex: 1.5,
-    borderTopLeftRadius: 30,
-    borderTopRightRadius: 30,
-    paddingVertical: 50,
-    paddingHorizontal: 50,
   },
   textRegister: {
     color: '#111219',
     width: 300
   },
-  textLogin: {
-    backgroundColor: '#fff',
-    marginTop: 480,
-    width: 300
+  list: {
+    marginTop: 70
+  },
+  item: {
+    padding: 16,
+    marginVertical: 1,
+    marginHorizontal: 0,
+    borderBottomColor: '#343434',
+    borderBottomWidth: 0.2,
+    flexDirection: 'row'
+  },
+  leftViewItem: {
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+  rightViewItem: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+  textRowList: {
+    color: '#FFFF',
+    fontSize: 19,
   }
+
 })
 
