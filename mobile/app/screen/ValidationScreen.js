447f6139b6e4d8c8182c2349118ba7726d16fc2f
Gandalf0u <leo.auvray@free.fr>
2021-06-02 22:14:24 +0200

    validation screen requete

--- mobile/app/screen/ValidationScreen.js
+++ mobile/app/screen/ValidationScreen.js
@@ -1,4 +1,4 @@
-import React  from 'react'
+import React, {useState} from 'react'
 import {
     ImageBackground,
     StyleSheet,
@@ -15,24 +15,36 @@ import { TextInput } from 'react-native-paper'
 import Button from '../components/Button'
 import BackButton from '../components/BackButton'
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import InputSpinner from 'react-native-input-spinner';
-import { SERVER_IP } from '@env';
+import InputSpinner from 'react-native-input-spinner'
+import { SERVER_IP } from '@env'
+import axios from 'axios'
+import * as SecureStore from "expo-secure-store"
+import DateTimePicker from '@react-native-community/datetimepicker'
 
-const test = AsyncStorage.getItem('cartSaved')
 
-// const cart =  SecureStore.getItemAsync('cartSaved')
 class ValidationScreen extends React.Component {
   constructor(){
     super();
     this.state = {
-        items: [],
+      resNumber: null,
+      onSite: false,  //remplacer les deux bouton par un checker qui modifie le state de onSite en true ou false ? à toi de voir Seb pour dire quand c'est sur place ou non t'as juste a modifier ce state en true ou false
+      time: null,
+      cost: 0.00,
+      items: [],
+      comment: "",
     }
-    // this.GetCard = this.GetCard.bind(this);
   }
   getCard = async() =>{
     const cart =  await AsyncStorage.getItem('cartSaved')
     .then(cart => {
-      this.setState({items : JSON.parse(cart)})
+      const cartParsed = JSON.parse(cart)
+      var cost = 0.00
+      this.setState({items : cartParsed})
+      cartParsed.forEach(element => {
+        cost += element.quantité  //j'utilise la quantité pour le cost pasque t'as pas stocké le cost dans les items et je sais plus où c'est
+      })
+      this.setState({cost : cost})
+      console.log(this.state.cost)
     })
   }
 
@@ -47,17 +59,49 @@ class ValidationScreen extends React.Component {
     this._unsubscribe();
   }
 
+  commentHandler = (text) => {
+    this.setState({comment: text})
+  }
+
+  dateHandler = (event, date) => {
+    this.setState({date: date})
+  }
+
+  handleSubmit(){
+    this.validate(this.state.resNumber, this.state.onSite, this.state.time, this.state.cost, this.state.items, this.state.comment)
+  }
+
+  validate = async(resNumber, onSite, time, cost, items, comment) =>{
+    const token = await SecureStore.getItemAsync('secure_token')
+    const config = {
+      headers: { Authorization: `Bearer ${token}` }
+    }
+    await axios.post(SERVER_IP + '/api/createOrder', {
+      //N°Reservation: resNumber,     stp change ce nom mdr
+      onSite: onSite,
+      hour: time,
+      Prix_Totale: cost,
+      Value: items,
+      comment: comment
+    }, config)
+    .then(response => {
+      console.log(response)
+    })
+    .catch(err => {
+      console.log(err.response)
+    })
+  }
+
      render(){
       const isFocused = this.props;
 
-      console.log(this.state.items)
         return(
             <View style={styles.container}>
             <SafeAreaView>
             <BackButton goBack={this.props.navigation.goBack}/>
             <View style={{alignItems: 'center', marginTop: 100}}>
                 <TextInput underlineColor="transparent" underlineColorAndroid="transparent" selectionColor='#5A5B61' style={styles.textLogin} label="Remarque (allergies, suppléments, ect...)"
-                       mode="flat" onChangeText={this.emailHandler}>
+                       mode="flat" onChangeText={this.commentHandler}>
                 </TextInput>
                 <TouchableOpacity
                         style={[styles.Button,{backgroundColor: this.state.middayHoursShow ? '#111219' : '#ffffff'}]} 
@@ -69,10 +113,18 @@ class ValidationScreen extends React.Component {
                         onPress={this.onMomentPressMidday}>
                         <Text style={[styles.TextButton,{color: this.state.middayHoursShow ? '#ffffff' : '#111219'}]}>À EMPORTER</Text>
                 </TouchableOpacity>
+                <DateTimePicker    //je sais pas sur IOS mais sur Android c'est une popup du coup faut pas la mettre ici je sais pas comment tu veux faire Seb
+                  value={new Date()} //rien de fonctionnel pour changer le format reçu directement dans le DateTimePicker faut changer après genre dans le date Handle
+                  mode={'time'}
+                  is24Hour={true}
+                  display="default"
+                  onChange={this.dateHandler}
+                  timeZoneOffsetInMinutes={120} //la date est en UTC, c'est sensé la mettre en UTC+2 mais jcp pk ça marche pas (a faire dans le date handler ?)
+                />
             </View>
             <View style={{alignItems: 'center', marginTop: 150}}>
                 <Button style={{width: 350}}  color='#111219'
-                    mode="outlined" onPress={this.handleSubmit} >
+                    mode="outlined" onPress={() => this.handleSubmit()} >
                         Valider la commande
                 </Button> 
             </View>
