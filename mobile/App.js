bc1674eb00276024b7c9a34b679645eb38470233
Sharkerz <maxence.lux@supinfo.com>
2021-06-07 10:23:41 +0200

    ✨ First try for notifications

--- mobile/App.js
+++ mobile/App.js
@@ -1,17 +1,19 @@
-import React, {useState} from 'react'
+import React, {useRef, useState} from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { theme } from './app/core/theme'
 import { Provider } from 'react-native-paper'
 import * as SecureStore from "expo-secure-store";
 const Stack = createStackNavigator()
-import { LogBox } from 'react-native';
+import {LogBox, Platform} from 'react-native';
 import Tabs from './app/naviguation/Tabs'
 import LoginScreen from './app/screen/LoginScreen'
 import RegistrationScreen from './app/screen/RegistrationScreen'
 import WelcomeScreen from './app/screen/WelcomeScreen'
 import {StatusBar} from "react-native";
 import Toast from 'react-native-toast-message';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
 
 // Status bar text color
 StatusBar.setBarStyle('light-content');
@@ -21,18 +23,56 @@ class App extends React.Component {
         super();
         this.state = {
             auth: false,
+            expoPushToken: "",
+            notification: false,
         };
+
+        Notifications.setNotificationHandler({
+            handleNotification: async () => ({
+                shouldShowAlert: true,
+                shouldPlaySound: false,
+                shouldSetBadge: false,
+            }),
+        });
     }
 
     componentDidMount() {
         this.getAuthStatus()
+        this.registerForPushNotificationsAsync().then((token) => this.setState({expoPushToken: token})).then(()=> console.log(this.state.expoPushToken));
+        Notifications.addNotificationReceivedListener(notification => {this.setState({notification: notification});
+        });
+    }
+
+    // Notifications set token
+    async registerForPushNotificationsAsync() {
+        let token;
+            const {status: existingStatus} = await Notifications.getPermissionsAsync();
+            let finalStatus = existingStatus;
+            if (existingStatus !== 'granted') {
+                const {status} = await Notifications.requestPermissionsAsync();
+                finalStatus = status;
+            }
+            if (finalStatus !== 'granted') {
+                alert('Failed to get push token for push notification!');
+                return;
+            }
+            token = (await Notifications.getExpoPushTokenAsync()).data;
+        if (Platform.OS === 'android') {
+            await Notifications.setNotificationChannelAsync('default', {
+                name: 'default',
+                importance: Notifications.AndroidImportance.MAX,
+                vibrationPattern: [0, 250, 250, 250],
+                lightColor: '#FF231F7C',
+            });
+        }
+        return token;
     }
 
     // get auth status
     getAuthStatus = async () => {
         const token = await SecureStore.getItemAsync('secure_token')
         if (token !== null) {
-            user = await SecureStore.getItemAsync('user')
+            let user = await SecureStore.getItemAsync('user')
             this.setState({user: JSON.parse(user)})
             this.setState({auth: true})   
         }
