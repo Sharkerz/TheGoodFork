a821b21559bf9581925aa8474178f4598f7188d8
Sharkerz <maxence.lux@supinfo.com>
2021-06-09 15:37:37 +0200

    ✨ Add front test chartJs

--- web/resources/views/Home.blade.php
+++ web/resources/views/Home.blade.php
@@ -1,8 +1,24 @@
 @extends('layouts.app')
 
+@section('scripts')
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="{{ asset('js/home.js') }}"></script>
+@endsection
+
 @section('content')
     <h1>@lang('home.title_home')</h1>
 
 
+    <div>
+        <canvas id="myChart"></canvas>
+    </div>
+
+    <script>
+        // === include 'setup' then 'config' above ===
+        const myChart = new Chart(
+            document.getElementById('myChart'),
+            config
+        );
+    </script>
 
 @endsection
